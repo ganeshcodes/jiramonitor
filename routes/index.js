30709e6f8c97869945dbbac2ac210b8faa58ebbc
Ganeshbabu Thavasimuthu <ganeshcodes2@gmail.com>
2019-02-20 21:51:30 -0600

    update

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.post('/issue_created', function(req, res, next) {
+  res.send('New JIRA issue created');
+});
+
 module.exports = router;
